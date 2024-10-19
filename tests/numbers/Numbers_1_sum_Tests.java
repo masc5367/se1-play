2f5ec48d8d5cfccc5e2ef521c04eaa288f0ffd9d
Sven Graupner <sgraupner@bht-berlin.de>
2024-10-19 23:23:46 +0200

    add JUnit5 tests, README.md

---
+++ tests/numbers/Numbers_1_sum_Tests.java
@@ -0,0 +1,58 @@
+package numbers;
+
+import application.Runtime;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * JUnit 5 test class of Numbers class, sum() method.
+ * 
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class Numbers_1_sum_Tests {
+
+    /*
+     * tested object is an instance of the Numbers class
+     */
+    private static Numbers testObj;
+
+
+    /**
+     * Static setup method executed once for all tests. Creates
+     * the test object.
+     * 
+     * @throws Exception creation of tested object failed
+     */
+    @BeforeAll
+    public static void setUpBeforeClass() throws Exception {
+        testObj = Runtime.getInstance().getBean(Numbers.class)
+            .orElseThrow(() -> new Exception(String.format(
+                "no test object for: %s", Numbers.class.getName())));
+    }
+
+    @Test
+    @Order(100)
+    void test100_sum_regular() {
+        assertEquals(30, testObj.sum(NumbersImpl.numbers));
+    }
+
+    @Test
+    @Order(101)
+    void test101_sum_regular() {
+        assertEquals(50, testObj.sum(NumbersImpl.numb_1));
+    }
+
+    @Test
+    @Order(102)
+    void test102_sum_regular() {
+        assertEquals(10984, testObj.sum(NumbersImpl.numb_2));
+    }
+
+    @Test
+    @Order(103)
+    void test103_sum_regular() {
+        assertEquals(141466, testObj.sum(NumbersImpl.numb_3));
+    }
+}
